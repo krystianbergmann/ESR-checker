18cb17813e3b1d23573050e2b05975e1269045e9
krystianbergmann <krystian.bergmann@gmail.com>
2026-03-27 00:33:17 +0100

    New files structure and test files

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-esr_table = [
-    {"capacitance": 1, "esr_max": 15.0, "esr_audio": 10.0},
-    {"capacitance": 2.2, "esr_max": 10.0, "esr_audio": 6.0},
-    {"capacitance": 4.7, "esr_max": 5.0, "esr_audio": 3.0},
-    {"capacitance": 10, "esr_max": 3.0, "esr_audio": 2.0},
-    {"capacitance": 22, "esr_max": 2.0, "esr_audio": 1.0},
-    {"capacitance": 47, "esr_max": 1.0, "esr_audio": 0.7},
-    {"capacitance": 100, "esr_max": 0.8, "esr_audio": 0.5},
-    {"capacitance": 220, "esr_max": 0.5, "esr_audio": 0.3},
-    {"capacitance": 470, "esr_max": 0.3, "esr_audio": 0.2},
-    {"capacitance": 1000, "esr_max": 0.2, "esr_audio": 0.12},
-    {"capacitance": 2200, "esr_max": 0.1, "esr_audio": 0.08}
-]
-
+from esr_data import esr_table
 while True:
     user_input = input("Enter capacitor value in uF (or 'exit'): ")
 
